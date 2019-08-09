93485b7fdc60a1785418d3122bb99249d359fc24
lipengbo <1589154771@qq.com>
2019-08-09 21:09:30 +0800

    第一次创建158后期项目

---
+++ src/test/java/com/lipengbo/Cmfz158ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.lipengbo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Cmfz158ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
